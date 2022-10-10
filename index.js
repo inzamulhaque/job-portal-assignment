63e1cbdf686911f907adebc4a7667eed91a3a2ac
inzamulhaque <mdihalif@gmail.com>
2022-10-10 19:19:01 +0600

    project setup

---
+++ index.js
@@ -0,0 +1,18 @@
+const mongoose = require("mongoose");
+const dotenv = require("dotenv").config();
+const colors = require("colors");
+const app = require("./app");
+
+// database connection
+const URI = `mongodb+srv://${process.env.DATABASE_USER}:${process.env.DATABASE_PASS}@cluster0.7kpjj.mongodb.net/${process.env.DATABASE_NAME}?retryWrites=true&w=majority`;
+
+mongoose.connect(URI).then(() => {
+  console.log(`Database connection is successful 🛢`.blue.bold);
+});
+
+// server
+const port = process.env.PORT || 7000;
+
+app.listen(port, () => {
+  console.log(`http://localhost:${port}`.yellow.bold);
+});
